7d11425612af300ee9ad0cc01257f5152dc7e66c
Matthew Garrett <matthew.garrett@nebula.com>
2013-08-09 18:36:30 -0400

    UBUNTU: SAUCE: UEFI: Add option to automatically enforce module signatures when in Secure Boot mode
    BugLink: http://bugs.launchpad.net/bugs/1566221
    
    UEFI Secure Boot provides a mechanism for ensuring that the firmware will
    only load signed bootloaders and kernels. Certain use cases may also
    require that all kernel modules also be signed. Add a configuration option
    that enforces this automatically when enabled.
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1160,6 +1160,13 @@ void __init setup_arch(char **cmdline_p)
 
 	io_delay_init();
 
+#ifdef CONFIG_EFI_SECURE_BOOT_SIG_ENFORCE
+	if (boot_params.secure_boot) {
+		enforce_signed_modules();
+		pr_info("Secure boot enabled\n");
+	}
+#endif
+
 	/*
 	 * Parse the ACPI tables for possible boot-time SMP configuration.
 	 */
