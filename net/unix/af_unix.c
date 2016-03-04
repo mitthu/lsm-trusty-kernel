623ed3337ac54f2a35c7c199e74dbf1d9baa1f02
Joseph Salisbury <joseph.salisbury@canonical.com>
2016-03-04 14:33:47 -0500

    Revert "af_unix: Revert 'lock_interruptible' in stream receive code"
    BugLink: http://bugs.launchpad.net/bugs/1540731
    
    This reverts commit d9ff970b0a5bde33ccdb6279001f9dcd95b68b95.
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -2112,7 +2112,14 @@ static int unix_stream_recvmsg(struct kiocb *iocb, struct socket *sock,
 		memset(&tmp_scm, 0, sizeof(tmp_scm));
 	}
 
-	mutex_lock(&u->readlock);
+	err = mutex_lock_interruptible(&u->readlock);
+	if (unlikely(err)) {
+		/* recvmsg() in non blocking mode is supposed to return -EAGAIN
+		 * sk_rcvtimeo is not honored by mutex_lock_interruptible()
+		 */
+		err = noblock ? -EAGAIN : -ERESTARTSYS;
+		goto out;
+	}
 
 	if (flags & MSG_PEEK)
 		skip = sk_peek_offset(sk, flags);
@@ -2153,7 +2160,8 @@ again:
 
 			timeo = unix_stream_data_wait(sk, timeo, last);
 
-			if (signal_pending(current)) {
+			if (signal_pending(current)
+			    ||  mutex_lock_interruptible(&u->readlock)) {
 				err = sock_intr_errno(timeo);
 				scm_destroy(siocb->scm);
 				goto out;
